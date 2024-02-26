5cc05100878be0dc5a2ab02c6585297841dbd3de
Geethu Joseph <geethujoseph03@gmail.com>
2024-02-26 19:37:40 +0530

    complete example notes app

---
+++ notes/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+const Footer = () =>{
+    const footerStyle = {
+        color:'green',
+        fonstStyle:'italic',
+        fontSize:16
+    }
+    return(
+        <div style={footerStyle}>
+            <br/>
+            <em>Note app. Created by yours truly.</em>
+        </div>
+    )
+}
+export default Footer;
